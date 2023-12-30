e75ccf3505ef807f52fe981e654c761051ff49bf
Andrew Gerez <andrewgerez18@gmail.com>
2023-12-30 01:53:05 -0300

    feat: 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "@/components/Link";
+
+export default function Page404() {
+  return (
+    <div>
+      <h1>You got lost and landed on page 404.</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
+  )
+}
