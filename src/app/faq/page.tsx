addbc62737b95f8cc3578a06016b7042f8e6371b
Andrew Gerez <andrewgerez18@gmail.com>
2023-12-30 01:38:09 -0300

    feat: pages

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FAQ() {
+  return (
+    <div>
+      <h1>Next Cases - FAQ Page</h1>
+      <Link href="/">Back to Home</Link>
+    </div>
+  )
+}
