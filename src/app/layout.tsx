0ddd3450d572f9903c3a5a41a096a0014556fcca
Andrew Gerez <andrewgerez18@gmail.com>
2023-12-30 22:18:34 -0300

    feat: registry styled-components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,5 @@
+import StyledComponentsRegistry from '@/lib/registry'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -15,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>
+        <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
